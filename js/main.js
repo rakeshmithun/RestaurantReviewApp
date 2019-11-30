d92496881570f383ab4df96fc2ca9e0f8d5ac5b2
Rakesh Mithun <slipknut@gmail.com>
2019-11-30 18:38:42 +1000

    30112019_634pm

--- js/main.js
+++ js/main.js
@@ -68,35 +68,18 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 /**
  * Initialize leaflet map, called from HTML.
  */
-initMap = () => {
-        self.newMap = L.map('map', {
-            center: [40.722216, -73.987501],
-            zoom: 12,
-            scrollWheelZoom: false
-        });
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-            mapboxToken: 'pk.eyJ1Ijoic291bXlhMDQ0IiwiYSI6ImNqa3Vra2RuNDA3b2YzcG1xc3JzdXZ5MXcifQ.wzTHtX-IX4tRVQzU0X4E4A',
-            maxZoom: 18,
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            id: 'mapbox.streets'
-        }).addTo(newMap);
-
-        updateRestaurants();
-    }
-    /* window.initMap = () => {
-      let loc = {
+window.initMap = () => {
+    let loc = {
         lat: 40.722216,
         lng: -73.987501
-      };
-      self.map = new google.maps.Map(document.getElementById('map'), {
+    };
+    self.map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
         center: loc,
         scrollwheel: false
-      });
-      updateRestaurants();
-    } */
+    });
+    updateRestaurants();
+}
 
 /**
  * Update page and map for current restaurants.
